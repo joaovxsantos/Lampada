f7114a6a3e5529a906ffc3149a59879d8a4b7fb1
João Vitor <joaovitor55670@gmail.com>
2022-07-01 15:34:08 -0300

    quarta commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
+//variáveis
 let img = document.getElementsByTagName("img")[0];
 let ligar = document.getElementsByTagName("button")[0]
 let desligar = document.getElementsByTagName("button")[1]
 
+//passando dom para as variáveis
 img.addEventListener("mouseover", trade1)
 img.addEventListener("mouseout", trade2)
 img.addEventListener("dblclick", broke)
@@ -11,20 +14,20 @@ desligar.addEventListener("click", trade2)
 
 
 
-
-function trade1(){
+//funçoes para utilizar nas variáveis
+function trade1() {
     img.src = "./images/ligada.jpg"
 }
 
 
-function trade2(){
+function trade2() {
     img.src = "./images/desligada.jpg"
 }
 
 
-function broke(){
+function broke() {
     img.src = "./images/quebrada.jpg"
-    
+
     img.removeEventListener("mouseover", trade1)
     img.removeEventListener("mouseout", trade2)
 }
