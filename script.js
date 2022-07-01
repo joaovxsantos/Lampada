3ca52615274ff4238f516fd5129fca8493cddd00
João Vitor <joaovitor55670@gmail.com>
2022-07-01 15:10:03 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ let desligar = document.getElementsByTagName("button")[1]
 
 img.addEventListener("mouseover", trade1)
 img.addEventListener("mouseout", trade2)
+img.addEventListener("dblclick", broke)
 ligar.addEventListener("click", trade1)
 desligar.addEventListener("click", trade2)
 
 
 
 
+
 function trade1(){
-    img.src = "./images/on.png"
+    img.src = "./images/ligada.jpg"
 }
 
 
 function trade2(){
-    img.src = "./images/off.png"
+    img.src = "./images/desligada.jpg"
 }
 
+
+function broke(){
+    img.src = "./images/quebrada.jpg"
+    
+    img.removeEventListener("mouseover", trade1)
+    img.removeEventListener("mouseout", trade2)
+}
