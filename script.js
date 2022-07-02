87b0b3ba157ad4c6fedfb3bf800b5f06ee50c3c8
João Vitor <joaovitor55670@gmail.com>
2022-07-02 14:16:14 -0300

    quinto commit

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
 
 //variáveis
 let img = document.getElementsByTagName("img")[0];
-let ligar = document.getElementsByTagName("button")[0]
-let desligar = document.getElementsByTagName("button")[1]
-
-//passando dom para as variáveis
-img.addEventListener("mouseover", trade1)
-img.addEventListener("mouseout", trade2)
-img.addEventListener("dblclick", broke)
-ligar.addEventListener("click", trade1)
-desligar.addEventListener("click", trade2)
+let botoes = document.getElementById("botoes")
 
 
 
-
-//funçoes para utilizar nas variáveis
-function trade1() {
-    img.src = "./images/ligada.jpg"
+function OnOf(event){
+    colors[event.target.id]()
 }
 
 
-function trade2() {
-    img.src = "./images/desligada.jpg"
+
+let colors = {
+    "on":  () =>  img.src = "./images/ligada.jpg",
+    "off": () => img.src = "./images/desligada.jpg",
+    "broken": () => img.src = "./images/quebrada.jpg"
 }
 
 
-function broke() {
-    img.src = "./images/quebrada.jpg"
 
-    img.removeEventListener("mouseover", trade1)
-    img.removeEventListener("mouseout", trade2)
-}
+
+botoes.addEventListener("click", OnOf)
+img.addEventListener("click", () => {colors.broken()
+})
